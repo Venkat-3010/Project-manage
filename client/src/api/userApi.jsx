29c99da536232aaa212c6168ed97825b55dff3b6
Venkatesan N <akashvenkat47@gmail.com>
2024-06-18 00:58:31 +0530

    api setup

---
+++ client/src/api/userApi.jsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+const url = "http://localhost:4000";
+
+const registerUser = async({name, email, password}) => {
+    try {
+        const response = await axios.post(`${url}/register`, {
+            name,
+            email,
+            password
+        });
+        return response.data;
+    } catch (error) {
+        if(error.response && error.response.data.message === "User already exists"){
+            toast.warn('User already exists ', error.message, {
+              theme: 'dark',
+              position: 'bottom-right',
+            });
+          }else {
+            toast.warn('Something went wrong ', error.message, {
+              theme: 'dark',
+              position: 'bottom-right',
+            });
+          }
+    }
+};
+
+const loginUser = async({email, password}) => {
+    try {
+        const response = await axios.post(`${url}/login`, {
+            email,
+            password
+        });
+        if(response.data?.token){
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('email', response.data.email);
+            localStorage.setItem('password', response.data.password);
+            localStorage.setItem('id', response.data._id);
+        }
+        return response.data;
+    } catch (error) {
+        if(error.response && error.response.data.message === "User does not exist"){
+            toast.warn('User does not exist ', error.message, {
+              theme: 'dark',
+              position: 'bottom-right',
+            });
+          }else {
+            toast.warn('Something went wrong ', error.message, {
+              theme: 'dark',
+              position: 'bottom-right',
+            });
+          }
+    }
+};
+
+export { registerUser, loginUser }
--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
-const mongose = require("mongose");
+const mongose = require("mongoose");
 
 const connect = async () => {
   try {
